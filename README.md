56172bc2d6b19c1aa517686d1757d36b8940483a
Max <Maxhelbes@yandex.ru>
2024-12-21 17:00:58 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# desktop_tennis_score
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/desktop_tennis_score/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.desktop_tennis_score
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
